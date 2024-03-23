1961c00731f972a8cc4191d9c9ffc145dcc41dfb
Simón Jiménez Tamayo <65623835+sijita@users.noreply.github.com>
2024-03-23 17:37:22 -0500

    FEAT: Added mock data

---
+++ src/utils/mockData.ts
@@ -0,0 +1,71 @@
+export const videoFolders = [
+  {
+    name: "Afiliado master",
+    videos: "8",
+    size: "7.3 GB",
+    lastModification: "30/may/2023",
+  },
+  {
+    name: "BeMaster",
+    videos: "21",
+    size: "1.4 GB",
+    lastModification: "22/jun/2022",
+  },
+  {
+    name: "Comizzión",
+    videos: "10",
+    size: "284 MB",
+    lastModification: "11/sept/2021",
+  },
+  {
+    name: "Creador de Contenido",
+    videos: "2",
+    size: "4.3 GB",
+    lastModification: "21/jun/2023",
+  },
+  {
+    name: "Exportados Wil",
+    videos: "14",
+    size: "500 MB",
+    lastModification: "5/abr/2023",
+  },
+  {
+    name: "Afiliado master",
+    videos: "6",
+    size: "7.3 GB",
+    lastModification: "30/may/2023",
+  },
+];
+
+export const videoFiles = [
+  {
+    name: "Entrenamiento.mp4",
+    tamano: "7.3 GB",
+    duration: "4:11:37",
+    lastModification: "30/may/2023",
+  },
+  {
+    name: "Salida-al-mercado-version-nueva.mp4",
+    tamano: "1.4 GB",
+    duration: "47:55",
+    lastModification: "22/jun/2022",
+  },
+  {
+    name: "Como-cerrar-una-venta.mp4",
+    tamano: "284 MB",
+    duration: "10:12",
+    lastModification: "11/sept/2021",
+  },
+  {
+    name: "Crea-un-ticket-valioso.mp4",
+    tamano: "4.3 GB",
+    duration: "3:50:22",
+    lastModification: "21/jun/2023",
+  },
+  {
+    name: "Conquista-el-mercado-digital.mp4",
+    tamano: "500 MB",
+    duration: "5:00",
+    lastModification: "5/abr/2023",
+  },
+];
